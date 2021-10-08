07b21dfde59fede18a0ee5712ae4285f6ea9d114
eginurobi10@gmail.com <eginurobi10@gmail.com>
2021-10-08 18:11:01 +0700

    menambahka, modifikasi, edit_data.php

---
+++ application/controllers/kampus.php
@@ -0,0 +1,67 @@
+<?php
+class kampus extends CI_Controller{
+    
+    function_construct () {
+        parent::_construct();
+        $this->load->model ('m_data');
+        $this->load->helper ('url');
+    }
+
+    function index() {
+        $data ['mahasiswa'] = $this->m_data->tampil_data()->result();
+        $this->load->view('tampil_data',$data);
+    }
+
+    function tambah(){
+        $this->load_.view('input_data');
+    }
+
+    function tambah_aksi(){
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+        $pekerjaan = $thid->input->post('pekerjaan');
+
+        $data = array (
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan
+            );
+        $this->m_data->input_data($data,'mahasiswa');
+        redirect('kampus/index');
+        }
+    function edit($id){
+        $where = array<'id' => $id);
+        $data['mahasiswa'] = $this->m_data->edit_data($where,'mahasiswa')->result();
+        $this->load->view('edit_data',$data);
+    }
+
+    function update(){
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+        $pekerjaan = $thid->input->post('pekerjaan');
+    
+        $data = array (
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan
+        );
+            
+        $where = array(
+            'id' => $id
+        );
+
+        $this->m_data->update_data($where,$data,'mahasiswa');
+        redirect('kampus/index');
+    }
+
+    function hapus($id){
+        $where = array('id' => $id);
+        $this->m_data->hapus_data($where,'mahasiswa');
+        redirect('kampus/index');
+    
+    }
+    ?>
